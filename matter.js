cbb39f2ff9324dff5b078aaec82102452d205164
TimonAME <125364912+TimonAME@users.noreply.github.com>
2024-01-31 21:45:53 +0100

    Initial commit

---
+++ matter.js
@@ -0,0 +1,132 @@
+const matterContainer = document.querySelector("#matter-container");
+const THICCNESS = 400;
+
+// module aliases
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+  element: matterContainer,
+  engine: engine,
+  options: {
+    width: matterContainer.clientWidth,
+    height: matterContainer.clientHeight,
+    background: "transparent",
+    wireframes: false,
+    showAngleIndicator: false
+  }
+});
+
+// render balls
+let numberOfBalls = 200;
+let ballSize = 20;
+let delay = ballSize*2;
+
+for (let i = 0; i < numberOfBalls; i++ ) {
+  ((index) => {
+    setTimeout(() => {
+      // Random number between -20 and +20
+      let random = Math.floor(Math.random() * 60) - 30;
+      let circle = Bodies.circle(matterContainer.clientWidth / 2 + random, 10, ballSize, {
+        friction: 0.1,
+        frictionAir: 0.00001,
+        restitution: 0.8
+      });
+      Composite.add(engine.world, circle);
+    }, index * delay);
+  })(i);
+}
+
+var ground = Bodies.rectangle(
+  matterContainer.clientWidth / 2,
+  matterContainer.clientHeight + THICCNESS / 2,
+  27184,
+  THICCNESS,
+  { isStatic: true }
+);
+
+let leftWall = Bodies.rectangle(
+  0 - THICCNESS / 2,
+  matterContainer.clientHeight / 2,
+  THICCNESS,
+  matterContainer.clientHeight * 5,
+  {
+    isStatic: true
+  }
+);
+
+let rightWall = Bodies.rectangle(
+  matterContainer.clientWidth + THICCNESS / 2,
+  matterContainer.clientHeight / 2,
+  THICCNESS,
+  matterContainer.clientHeight * 5,
+  { isStatic: true }
+);
+
+// add all of the bodies to the world
+Composite.add(engine.world, [ground, leftWall, rightWall]);
+
+let mouse = Matter.Mouse.create(render.canvas);
+let mouseConstraint = Matter.MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false
+    }
+  }
+});
+
+Composite.add(engine.world, mouseConstraint);
+
+// allow scroll through the canvas
+mouseConstraint.mouse.element.removeEventListener(
+  "mousewheel",
+  mouseConstraint.mouse.mousewheel
+);
+mouseConstraint.mouse.element.removeEventListener(
+  "DOMMouseScroll",
+  mouseConstraint.mouse.mousewheel
+);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+function handleResize(matterContainer) {
+  // set canvas size to new values
+  render.canvas.width = matterContainer.clientWidth;
+  render.canvas.height = matterContainer.clientHeight;
+
+  // reposition ground
+  Matter.Body.setPosition(
+    ground,
+    Matter.Vector.create(
+      matterContainer.clientWidth / 2,
+      matterContainer.clientHeight + THICCNESS / 2
+    )
+  );
+
+  // reposition right wall
+  Matter.Body.setPosition(
+    rightWall,
+    Matter.Vector.create(
+      matterContainer.clientWidth + THICCNESS / 2,
+      matterContainer.clientHeight / 2
+    )
+  );
+}
+
+window.addEventListener("resize", () => handleResize(matterContainer));
